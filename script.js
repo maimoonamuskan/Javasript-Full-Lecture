37adc947db4b6ff50ded48606a066c31a0e35454
maimoonamuskan <maimoonamuskan89@gmail.com>
2026-08-05 12:39:36 -0700

    add files of portfolio

---
+++ script.js
@@ -0,0 +1,61 @@
+﻿
+
+AOS.init({
+    duration: 1000,
+    once: false
+});
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+
+    if (header) {
+        header.classList.toggle("sticky", window.scrollY > 20);
+    }
+});
+
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation a");
+
+if (menuBtn && navigation) {
+
+    menuBtn.addEventListener("click", () => {
+        menuBtn.classList.toggle("active");
+        navigation.classList.toggle("active");
+    });
+
+    navigationItems.forEach((item) => {
+        item.addEventListener("click", () => {
+            menuBtn.classList.remove("active");
+            navigation.classList.remove("active");
+        });
+    });
+
+}
+
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+if (scrollBtn) {
+
+    window.addEventListener("scroll", () => {
+
+        if (window.scrollY > 500) {
+            scrollBtn.classList.add("active");
+        } else {
+            scrollBtn.classList.remove("active");
+        }
+
+    });
+
+    scrollBtn.addEventListener("click", () => {
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+
+    });
+
+}
+
+
